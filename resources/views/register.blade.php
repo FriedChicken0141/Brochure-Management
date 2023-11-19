e82c0277eaf620f1c26810a40aa762b24e252de8
akasaka-yuri <129761690+akasaka-yuri@users.noreply.github.com>
2023-11-20 00:00:44 +0900

    新規登録画面にプレビューを実装

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-10">
+        <div class="col-md-12">
             @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -23,39 +23,43 @@
                 <form method="POST" action="/brochures/add" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body">
-                        <div class="form-group">
-                            <label for="name">パンフレット名</label>
-                            <input type="text" class="form-control" name="name" required>
-                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">パンフレット名</label>
+                                <input type="text" class="form-control" name="name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="area">該当市町（県）</label>
+                                <select type="area" class="form-control" name="area_id" required>
+                                    {{-- valueの中を空にすることで空欄を選択することをできなくする --}}
+                                    <option value=""disabled selected>選択してください</option>
+                                    @foreach($areas as $area)
+                                        <option value="{{ $area->id }}">{{ $area->area_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="detail">残数</label>
+                                <input type="text" class="form-control" name="quantity" required>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="area">該当市町（県）</label>
-                            <select type="area" class="form-control" name="area_id" required>
-                                {{-- valueの中を空にすることで空欄を選択することをできなくする --}}
-                                <option value=""disabled selected>選択してください</option>
-                                @foreach($areas as $area)
-                                    <option value="{{ $area->id }}">{{ $area->area_name }}</option>
-                                @endforeach
-                            </select>
+                            <div class="form-group">
+                                <label for="detail">詳細</label>
+                                <input type="text" class="form-control" name="detail" >
+                            </div>
+                            <div class="form-group">
+                                <div><label for="cover">表紙画像</label></div>
+                                <input type="file" class="form-image" name="image" accept="image/*" id="form-image">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">登録</button>
+                            </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="detail">残数</label>
-                            <input type="text" class="form-control" name="quantity" required>
+                        <div class="col-md-6">
+                            <div class="preview">
+                                <img id="preview" class="image-preview">
+                            </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="detail">詳細</label>
-                            <input type="text" class="form-control" name="detail" >
-                        </div>
-
-                        <div class="form-group">
-                            <div><label for="cover">表紙画像</label></div>
-                            <input type="file" class="form-image" name="image" >
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">登録</button>
                     </div>
                 </form>
             </div>
@@ -64,7 +68,21 @@
 @stop
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('/css/style-register.css')  }}" >
 @stop
 
 @section('js')
+<script>
+    $('#form-image').on('change', function(){
+	var $fr = new FileReader();
+	$fr.onload = function(){
+		$('#preview').attr('src', $fr.result);
+
+        $('.image-preview').addClass('add-solid');
+	}
+	$fr.readAsDataURL(this.files[0]);
+
+
+});
+</script>
 @stop
